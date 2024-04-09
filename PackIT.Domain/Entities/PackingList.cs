d65d6a15cd86a088f2e19a276e971be34471636a
MQ29 <matiax2829@gmail.com>
2024-04-09 18:58:43 +0200

    PackItem, RemoveItem

--- PackIT.Domain/Entities/PackingList.cs
+++ PackIT.Domain/Entities/PackingList.cs
@@ -19,7 +19,12 @@ namespace PackIT.Domain.Entities
 
         private readonly LinkedList<PackingItem> _items = new();
 
-        internal PackingList(Guid id, PackingListName name, Localization localization, LinkedList<PackingItem> items)
+        internal PackingList(PackingListId id, PackingListName name, Localization localization, LinkedList<PackingItem> items) : this(id, name, localization) 
+        {
+            _items = items; //we are assuming that there is no dublicates, we are not using AddItem() 
+        }
+
+        internal PackingList(PackingListId id, PackingListName name, Localization localization)
         {
             Id = id;
             _name = name;
@@ -37,6 +42,42 @@ namespace PackIT.Domain.Entities
 
             _items.AddLast(item);
             AddEvent(new PackingItemAdded(this,item));
+        }
+
+        public void AddItems(IEnumerable<PackingItem> items)
+        {
+            foreach (var item in items)
+            {
+                AddItem(item);
+            }
+        }
+
+        public void PackItem(string itemName)
+        {
+            var item = GetItem(itemName);
+            var packedItem = item with { IsPacked = true };
+
+            _items.Find(item).Value = packedItem;
+            AddEvent(new PackingItemAdded(this, item));
+        }
+
+        public void RemoveItem(string itemName)
+        {
+            var item = GetItem(itemName);
+            _items.Remove(item);
+            AddEvent(new PackingItemRemoved(this, item));
+        }
+
+        private PackingItem GetItem(string itemName)
+        {
+            var item = _items.SingleOrDefault(x => x.Name == itemName);
+
+            if (item is null)
+            {
+                throw new PackingItemNotFoundException(itemName);
+            }
+
+            return item;
         }
     }
 }
