744bbd8d355b3bc3bf5785cb7734032994f87810
MQ29 <matiax2829@gmail.com>
2024-04-21 20:53:29 +0200

    Query handler

---
+++ PackIT.Application/Queries/Handlers/GetPackingListHandler.cs
@@ -0,0 +1,31 @@
+﻿using PackIT.Application.DTO;
+using PackIT.Application.Exceptions;
+using PackIT.Domain.Repositories;
+using PackIT.Shared.Abstractions.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackIT.Application.Queries.Handlers
+{
+    internal sealed class GetPackingListHandler : IQueryHandler<GetPackingList, PackingListDto>
+    {
+        private readonly IPackingListRepository _repository;
+        public GetPackingListHandler(IPackingListRepository repository)
+            => _repository = repository;
+
+        public async Task<PackingListDto> HandleAsync(GetPackingList query)
+        {
+            var packingList = await _repository.GetAsync(query.Id);
+
+            if (packingList is null)
+            {
+                throw new PackingListDoesntExistException(query.Id);
+            }
+
+            return packingList?.AsDto();
+        }
+    }
+}
---
+++ PackIT.Application/Queries/Handlers/SearchPackingListHandler.cs
@@ -0,0 +1,17 @@
+﻿using PackIT.Application.DTO;
+using PackIT.Shared.Abstractions.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackIT.Application.Queries.Handlers
+{
+    public class SearchPackingListHandler : IQueryHandler<SearchPackingLists, IEnumerable<PackingListDto>>
+    {
+        public Task<IEnumerable<PackingListDto>> HandleAsync(SearchPackingLists query)
+        {
+        }
+    }
+}
